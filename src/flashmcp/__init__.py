5845df2e20473a813ba877bbf748ee5883681725
FlashMCP <mounir.mkhallati@gmail.com>
2024-11-30 19:53:46 -0500

    Use hatchling

--- src/flashmcp/__init__.py
+++ src/flashmcp/__init__.py
@@ -1,6 +1,8 @@
 """FlashMCP - A more ergonomic interface for MCP servers."""
 
+from importlib.metadata import version
 from .server import FlashMCP, Context
 from .utilities.types import Image
 
+__version__ = version("FlashMCP")
 __all__ = ["FlashMCP", "Context", "Image"]
